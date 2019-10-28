13b6e571301e1c80e2a18f012bacc4c5d2fcb105
Amanuel Goshu <goshu@amanuels-mbp.home>
2019-10-27 21:02:46 -0400

    Week 1 Solution Amanuel N square runtime

--- week_1_Google_array_aman.java
+++ week_1_Google_array_aman.java
@@ -11,26 +11,73 @@
  * ##################################################################################
 */
 
-/**
- * ##################################################################################
- * Problem extension ...
- * Bonus: Can you do this in one pass i.e using only one loop?
- * ##################################################################################
- */
-
-
 // ################################# Amanuel's Solution #############################
+class Main {  
 
-// ################################## Test Case #1 ##################################
+    // method to find if k is sum of any two numbers
+    public static boolean SumFinder(Integer[] numbers, Integer k) {
+      boolean flag = false;
+      
+      // runtime of N squared.
+      for (int i =0; i < numbers.length; i++){
+        for (int j=0; j< i; j++){
+          if (numbers[i] + numbers[j] == k){
+            flag = true;
+          }
+        }
+      }
+  
+      return flag;
+    }
 
-// ################################## Test Case #2 ##################################
+    /**
+     * ##################################################################################
+     * Problem extension ...
+     * Bonus: Can you do this in one pass i.e using only one loop?
+     * ##################################################################################
+     */
 
-// ################################## Test Case #3 ##################################
 
-// ################################## Test Case #4 ##################################
+  
+    public static void main(String[] args) {
+      Integer[] myArry = {10, 15, 3, 7};
+      
+      // First Solution Tests
+      // ################################## Test Case #1 ##################################
+      int k1 = 17;
+      boolean test1 = SumFinder(myArry, k1);
+      System.out.println(test1);
+      // ################################## Test Case #2 ##################################
+      int k2 = 10;
+      boolean test2 = SumFinder(myArry, k2);
+      System.out.println(test2);
+      // ################################## Test Case #3 ##################################
+      int k3 = 13;
+      boolean test3 = SumFinder(myArry, k3);
+      System.out.println(test3);
+      // ################################## Test Case #4 ##################################
+      int k4 = 3;
+      boolean test4 = SumFinder(myArry, k4);
+      System.out.println(test4);
+      // ################################## Test Case #5 ##################################
+      int k5 = 30;
+      boolean test5 = SumFinder(myArry, k5);
+      System.out.println(test5);
 
-// ################################## Test Case #5 ##################################
+      // Linear Algorithmic Time Solution - Tests
+      // ################################## Test Case #1 ##################################
 
+      // ################################## Test Case #2 ##################################
+
+      // ################################## Test Case #3 ##################################
+
+      // ################################## Test Case #4 ##################################
+      
+      // ################################## Test Case #5 ##################################
+
+    }
+    
+  }
 /**
 * Topics to review
 * Loops, Arrays and Array Operations
